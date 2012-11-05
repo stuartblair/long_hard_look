fb638c2c2147c961963f4a4a00e7781109c1b55e
Stuart Blair <stuart.a.blair@gmail.com>
2012-11-04 21:42:45 -0800

    First passing scenario. Since it's the first outside-in test, we've not gone as far as driving out a unit test. This is really more about getting our walking skeleton up and out of its coffin.

--- lib/long_hard_look/long_hard_look.rb
+++ lib/long_hard_look/long_hard_look.rb
@@ -1,11 +1,40 @@
 module LongHardLook
 	class LongHardLook
-		puts <<OUTPUT
+	
+		workspace = ARGV
+		
+		def self.path_prefix
+			"#{File.dirname(__FILE__)}/../../"
+		end
+
+		def self.java_file_from(path)
+			/test_workspace\/(.+?)\/.*\/(.+?)$/.match(Dir.glob("test_workspace/**/*.java")[0])[2]
+		end
+
+		def self.package_from(path)
+			package = ""
+			file_contents = ""
+			IO.readlines(path).each {|line| file_contents << line}
+			package = /\s*package (.+);/.match(file_contents)[1]
+		end
+
+		def self.application_name()
+			"AppA"
+		end
+
+		app_to_java_file = {}
+
+		Dir.glob("#{path_prefix}/test_workspace/**/*.java").each do |java_file|
+			app_to_java_file[application_name] = package_from(java_file)
+		end
+		
+		app_to_java_file.keys.each do |application_path|
+			puts <<OUTPUT
 Packages by application source tree
 ___________________________________
-AppA
-* org.service.components
+#{application_name}
+* #{app_to_java_file[application_name]}
 OUTPUT
+		end
 	end
 end
-
