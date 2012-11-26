2ba1808548aeeecbfc22c80ddb349cd3d33483d6
Stuart Blair <stuart.a.blair@gmail.com>
2012-11-26 20:09:31 +0000

    I don't like having to redefine constants even as part of the test. Silencing the constant redefinitions for ARGV in the unit tests just seemed like a bad smell. In this case, the fix was to allow TextUI to take a parameter to it initializer that would default to ARGV, but which could be set to any arbitrary array for the purposes of unit testsing.

--- lib/long_hard_look/text_ui.rb
+++ lib/long_hard_look/text_ui.rb
@@ -4,10 +4,10 @@ module LongHardLook
 	class TextUI
 		attr_reader :report, :format, :workspace
 
-		def initialize
-			options = Trollop::options do
+		def initialize(args=ARGV)
+			options = Trollop::options(args) do
 				opt :report, "Report mode", :type => :string
-				opt :format, "Report format", :type => :string, :default => "text"
+				opt :format, "Report format", :type => :string
 				opt :workspace, "Workspace location", :type => :string
 			end
 
--- spec/long_hard_look/text_ui_spec.rb
+++ spec/long_hard_look/text_ui_spec.rb
@@ -3,26 +3,20 @@ require 'long_hard_look/text_ui'
 module LongHardLook
 	describe TextUI do
 		context "when valid commandline is provided" do
-			around(:each) do |noisy_example|
-				#ARGV is a constant, so our test setup is going to result in some
-				#warnings. Let's temporarily disable the warnings for this context.
-				silence_warnings { noisy_example.run }
-			end
-
 			before(:each) do
-				ARGV = ["--report=package_report", "--format=text", "--workspace=/home/stuart/test_workspace"]
+				@text_ui = TextUI.new(["--report", "package_report", "--format", "text", "--workspace", "/home/stuart/test_workspace"])
 			end
 
 			it "maps the report type correctly" do
-				TextUI.new.report.should eql "package_report"
+				@text_ui.report.should eql "package_report"
 			end
 
 			it "maps the format type correctly" do
-				TextUI.new.format.should eql "text"
+				@text_ui.format.should eql "text"
 			end
 
 			it "maps the workspace correctly" do
-				TextUI.new.workspace.should eql "/home/stuart/test_workspace"
+				@text_ui.workspace.should eql "/home/stuart/test_workspace"
 			end
 		end
 	end
