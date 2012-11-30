34053e22447842d4ead05b9869413d65308270b0
Stuart Blair <stuart.a.blair@gmail.com>
2012-11-30 01:07:23 +0000

    Removed the duplication from the JavaApplication specs. There's definitely less code, but it still lacks something in expressing intent. Let's see what we come up with next.

--- spec/long_hard_look/java_application_spec.rb
+++ spec/long_hard_look/java_application_spec.rb
@@ -10,25 +10,21 @@ module LongHardLook
 		end
 
 		describe '#packages' do
+			before(:each) do
+				@filesystem_adapter = double('filesystem_adapter')
+				@java_application = JavaApplication.new(@filesystem_adapter, 'AppA')
+			end
+
 			context 'with a single Java class declared' do
 				it "returns the sole package" do
-					@filesystem_adapter = double('filesystem_adapter')
-					@java_file = double('java_file')
-					@java_file.stub(:contents).and_return("package org.test;")
-					@filesystem_adapter.stub(:find).with(:java).and_return([@java_file])
-					@java_application = JavaApplication.new(@filesystem_adapter, 'AppA')
+					@filesystem_adapter.stub(:find).with(:java).and_return([double(:contents => "package org.test;")])
 					@java_application.packages.should eql ["org.test"]
 				end
 			end
+
 			context 'with two Java classes in the same package' do
 				it "returns the sole package" do
-					@filesystem_adapter = double('filesystem_adapter')
-					@java_file_1 = double('java_file_1')
-					@java_file_1.stub(:contents).and_return("package org.test;")
-					@java_file_2 = double('java_file_2')
-					@java_file_2.stub(:contents).and_return("package org.test;")
-					@filesystem_adapter.stub(:find).with(:java).and_return([@java_file_1, @java_file_2])
-					@java_application = JavaApplication.new(@filesystem_adapter, 'AppA')
+					@filesystem_adapter.stub(:find).with(:java).and_return([double(:contents => "package org.test;")] * 2)
 					@java_application.packages.should eql ["org.test"]
 				end
 			end
